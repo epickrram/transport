b82846e8e3874c0ea27ce7e2c8198ece84d815d6
epickrram <mark@epickrram.com>
2018-02-06 19:25:14 +0000

    Add license header

--- src/test/java/com/aitusoftware/transport/integration/LocalIpcOneToOneIntegrationTest.java
+++ src/test/java/com/aitusoftware/transport/integration/LocalIpcOneToOneIntegrationTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 - 2018 Aitu Software Limited.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.aitusoftware.transport.integration;
 
 import com.aitusoftware.transport.Fixtures;
@@ -7,7 +22,7 @@ import com.aitusoftware.transport.factory.Service;
 import com.aitusoftware.transport.factory.ServiceFactory;
 import com.aitusoftware.transport.factory.SubscriberDefinition;
 import com.aitusoftware.transport.factory.SubscriberThreading;
-import com.aitusoftware.transport.messaging.StaticAddressSpace;
+import com.aitusoftware.transport.StaticAddressSpace;
 import com.aitusoftware.transport.messaging.TopicDispatcherRecordHandler;
 import com.aitusoftware.transport.messaging.proxy.PublisherFactory;
 import com.aitusoftware.transport.messaging.proxy.Subscriber;
